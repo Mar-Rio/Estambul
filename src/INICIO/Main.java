71355a3e2496c146e9d33a715d54b2ca1def3fb2
34601 <34601@DESKTOP-5JLQAOU>
2021-05-14 13:57:52 +0200

    Lo subo todo

---
+++ src/INICIO/Main.java
@@ -0,0 +1,33 @@
+package INICIO;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ * Clase en la que se ejecuta el programa
+ * 
+ * @author Adrian Pardo
+ * @version 1.0
+ * 
+ */
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../VISTA/inicio/ControllerInicio.fxml"));
+        primaryStage.setTitle("ESTAMBUL  TURISMO");
+        primaryStage.getIcons().add(new Image("/IMAGENES/pnglogo.png"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
